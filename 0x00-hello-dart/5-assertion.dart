ec32c8c416f5aa625cc184e45dc7425b2fa47135
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-24 10:00:48 +0000

    try to pass checker

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,4 +1,5 @@
 void main(List<String> args) {
-  assert(int.parse(args[0]) >= 80, "The score must be bigger or equal to 80");
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, "The score must be bigger or equal to 80");
   print("You passed");
 }
