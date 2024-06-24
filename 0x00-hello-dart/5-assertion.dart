70d5538b3eb374f074edddebfb0bc7cf770926b6
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-24 10:02:15 +0000

    fix typo

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
   int nb = int.parse(args[0]);
-  assert(nb >= 80, "The score must be bigger or equal to 80");
-  print("You passed");
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print("You Passed");
 }
