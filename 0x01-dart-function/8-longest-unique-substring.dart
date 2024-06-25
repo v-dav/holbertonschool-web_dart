633437a05c8d27f5711fd350863fbe283e2cf481
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-25 08:55:19 +0000

    try edge case empty string

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,4 +1,5 @@
 String longestUniqueSubstring(String str) {
+  if (str == "") return "";
   String substring = str[0];
   for (int i = 1; i < str.length; i++) {
     if (str[i] == substring || substring.contains(str[i])) {
