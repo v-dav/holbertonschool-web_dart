50d8c0eaa3c3d60540e8e470c6039dc9f8804c58
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-26 12:55:03 +0000

    write a class, validate password and override a method

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,16 @@
+class Password {
+  String password = '';
+
+  bool isValid() {
+    if (!(password.length >= 8 && password.length <= 16)) return false;
+    if (!(password.contains(RegExp(r'[A-Z]')) &&
+        password.contains(RegExp(r'[a-z]')))) return false;
+    if (!password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
