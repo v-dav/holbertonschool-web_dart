345783c40bb9c04184757eaa5f41f6887c787834
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-27 11:04:39 +0000

    try to pass checker

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,15 +1,20 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<void> printRmCharacters() async {
   try {
-    final response =
-        await http.get(Uri.parse("https://rickandmortyapi.com/api/character"));
-    final characters = jsonDecode(response.body)["results"];
-    for (final character in characters) {
-      print(character["name"]);
+    var response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      List<dynamic> characters = data['results'];
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      print('Failed to load characters: ${response.statusCode}');
     }
   } catch (error) {
-    print("error caught: $error");
+    print('error caught: $error');
   }
 }
