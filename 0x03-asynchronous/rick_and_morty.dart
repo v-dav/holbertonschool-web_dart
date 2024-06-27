c342fd2672c0289d907f970f88478e1648b50252
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-27 10:56:40 +0000

    print only first 20 characters name

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,23 +1,13 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-Future<void> printRmCharacters([String? url]) async {
+Future<void> printRmCharacters() async {
   try {
-    final endpoint = url ?? 'https://rickandmortyapi.com/api/character';
-    final response = await http.get(Uri.parse(endpoint));
-
-    if (response.statusCode == 200) {
-      Map<String, dynamic> responseBody = jsonDecode(response.body);
-      String? next = responseBody['info']['next'];
-      List<dynamic> characters = responseBody['results'];
-      characters.forEach((character) => print(character['name']));
-
-      // If there is a next page URL, recursively fetch the next page
-      if (next != null && next.isNotEmpty) await printRmCharacters(next);
-    } else {
-      print('Failed to load data. Status code: ${response.statusCode}');
-    }
+    final response =
+        await http.get(Uri.parse("https://rickandmortyapi.com/api/character"));
+    final characters = jsonDecode(response.body)["results"];
+    characters.forEach((character) => print(character["name"]));
   } catch (error) {
-    print('Error caught: $error');
+    print("error caught: $error");
   }
 }
