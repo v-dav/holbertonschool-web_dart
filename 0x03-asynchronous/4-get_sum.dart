84c453bf9dd57773c96c2a36d8ba754a54768960
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-06-27 10:08:58 +0000

    try to pass checker

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,14 +1,14 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<dynamic> calculateTotal() async {
   try {
     Map userData = jsonDecode(await fetchUserData());
     String userId = userData["id"];
     List userOrders = jsonDecode(await fetchUserOrders(userId));
     double total = 0;
 
-    for (var product in userOrders) {
+    for (final product in userOrders) {
       double price = double.parse(await fetchProductPrice(product));
       total += price;
     }
